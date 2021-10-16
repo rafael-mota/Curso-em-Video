f753a53d1c75f4875d7aec7830647db091e1fdba
rafael-mota <rafael.f.s.mota@gmail.com>
2021-10-15 23:44:26 -0300

    Ex069 modificado e concluído.

--- Python - Curso em Video/Ex069.py
+++ Python - Curso em Video/Ex069.py
@@ -1,29 +1,26 @@
-#Cadastro e analise de dados com while
-
-contador_maioridade = 0
-contador_masculino = 0
-contador_feminino_menor = 0
+# Cadastro com análise de dados
+contador_maioridade = contador_masculino = contador_feminino_menor = 0
 
 while True:
     print(">>> CADASTRO PARA ANÁLISE <<<")
     idade = int(input("Idade: "))
-    sexo = str(input("Sexo [M/F]: "))[0]
+    sexo = " "
     while sexo not in "MmFf":
         sexo = str(input("Sexo [M/F]: "))[0]
-    continuar = str(input("Quer continuar? [S/N]: ")).strip()[0]
+    if idade > 18:
+        contador_maioridade += 1
+    if sexo in "Ff": 
+        if idade < 20:
+            contador_feminino_menor += 1
+    else:
+        contador_masculino += 1    
+    continuar = " "
     while continuar not in "SsNn":
         continuar = str(input("Quer continuar? [S/N]: ")).strip()[0]
-    if continuar not in "Ss":
+    if continuar in "Nn":
         break
-    else:
-        if idade >= 18:
-            contador_maioridade += 1
-        if sexo in "Mm":
-            contador_masculino += 1
-        elif sexo in "Ff" and idade < 20:
-            contador_feminino_menor += 1
-
 print(f"PROCESSO DE CADASTRO ENCERRADO. ")
 print(f"{contador_maioridade} pessoas cadastradas tem mais de 18 anos;")
 print(f"{contador_masculino} Homens foram cadastrados;")
 print(f"{contador_feminino_menor} Mulheres tem menos de 20 anos")
+ 
