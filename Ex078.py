e31f12caf60e632daac7b5372b1d765992491377
rafael-mota <rafael.f.s.mota@gmail.com>
2021-11-09 01:48:25 -0300

    Ex078 modificado.

--- Ex078.py
+++ Ex078.py
@@ -1,21 +1,32 @@
 #Check de valores - MAIOR E MENOR COM INPUT
 lista = []
 minimo = 0
-indice_min = 0
 maximo = 0
-indice_max = 0
-for valor in range(5):
-    lista.append(int(input(f"Digite um valor para a posição {valor}: ")))
+indice_min = []
+indice_max = []
+for posicao in range(5):
+    lista.append(int(input(f"Digite um valor para a posição {posicao}: ")))
 for indice, valor in enumerate(lista):
     if minimo == 0 and maximo == 0:
-        minimo = maximo = valor
+       minimo = maximo = valor
+       indice_max.append(indice)
+       indice_min.append(indice)
     elif valor < minimo:
         minimo = valor
-        indice_min = indice
+        indice_min.clear()
+        indice_min.append(indice)
     elif valor > maximo:
         maximo = valor
-        indice_max = indice
-
+        indice_max.clear()
+        indice_max.append(indice)
+    elif valor == minimo:
+        indice_min.append(indice)
+    elif valor == maximo:
+        indice_max.append(indice)
 print(f"Você digitou os valores {lista}")
-print(f"O valor maior valor da lista foi {max(lista)} e está no indice {lista.index(max(lista))}")
-print(f"O valor menor valor da lista foi {min(lista)} e está no indice {lista.index(min(lista))}")
+print(f"O maior valor da lista foi {maximo} e está no indice", end=" ")
+for index in indice_max:
+    print(f"{index}...", end=" ")
+print(f"\nO menor valor da lista foi {minimo} e está no indice", end=" ")
+for index in indice_min:
+    print(f"{index}...", end=" ")
